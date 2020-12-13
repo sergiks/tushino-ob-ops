ae34f4fbe1741035746be0c1ef029b5f7a0bbf62
Sergei Sokolov <hello@sergeisokolov.com>
2020-12-13 12:19:07 +0300

    tidy up

--- pub-slush.py
+++ pub-slush.py
@@ -1,67 +1,67 @@
 #!/bin/python
 # -*- coding: utf-8 -*-
 
-files = [
-  'ZaklucheniePZZ-02397-2020-EOO02-11-202018_13(0).txt', # 494/20
-  'ZaklucheniePPT-003085-EOO05-11-202014_16(0)(1).txt',  # 526/20
-  'ZaklucheniePPT-003082-EOO05-11-202009_32(0).txt',     # 527/20
-  'ZaklucheniePZZ-02395-2020-EOO02-11-202018_33(0).txt', # 528/20
-]
-
-file = open(files[3], 'r')
-lines = file.readlines()
-file.close()
-
-expect = 1
-cursor = 0
-result = {
-    "yes-ok" : 0,
-    "yes-but": 0,
-    "other"  : 0,
-    "not"    : [],
+files = {
+  '494/20': 'ZaklucheniePZZ-02397-2020-EOO02-11-202018_13(0).txt',
+  '526/20': 'ZaklucheniePPT-003085-EOO05-11-202014_16(0)(1).txt', 
+  '527/20': 'ZaklucheniePPT-003082-EOO05-11-202009_32(0).txt',    
+  '528/20': 'ZaklucheniePZZ-02395-2020-EOO02-11-202018_33(0).txt',
 }
 
-limit = 3000000
 
-def get_line(n):
-    if len(lines) <= n:
-        return ''
-    return lines[n].strip()
+def analyze_file(id, filename):
+    file = open(filename, 'r')
+    lines = file.readlines()
+    file.close()
 
-def log(cursor, text):
-    print("{}: {}".format(cursor, text)) 
+    expect = 1
+    cursor = 0
+    result = {
+        "yes-ok" : 0,
+        "yes-but": 0,
+        "not"    : 0,
+        "not-ids": [],
+    }
+
+    def get_line(n):
+        if len(lines) <= n:
+            return ''
+        return lines[n].strip()
+
+    def log(cursor, text):
+        print("{}: {}".format(cursor, text)) 
 
 
-while True: 
-    cursor += 1
-    if cursor >= len(lines):
-        print("All done")
-        break
+    while True: 
+        cursor += 1
+        if cursor >= len(lines):
+            break
 
-    # wait for response number alone on the line
-    text = get_line(cursor)
-    if text == str(expect):
-        # log(cursor, text) 
-        expect = expect + 1
-        cursor += 2
-
+        # wait for response number alone on the line
         text = get_line(cursor)
+        if text == str(expect):
+            expect = expect + 1
+            cursor += 2
 
-        if text == "Поддерживаю":
-            text = get_line(cursor + 2)
-            if text == "Рекомендовано":
-                result['yes-ok'] += 1
-                cursor += 2
+            text = get_line(cursor)
+
+            if text == "Поддерживаю":
+                text = get_line(cursor + 2)
+                if text == "Рекомендовано":
+                    result['yes-ok'] += 1
+                    cursor += 2
+                else:
+                    result['yes-but'] += 1
             else:
-                result['yes-but'] += 1
-        else:
-            result['other'] += 1
-            result['not'].append(expect - 1)
-            print("  №{} строка {}: {}".format(expect-1, text, cursor))
+                result['not'] += 1
+                result['not-ids'].append(expect - 1)
+                # print("  №{} строка {}: {}".format(expect-1, text, cursor))
 
-    if cursor > limit:
-        print("Cursor over {}, breaking".format(limit))
-        break
+    print("\nОбсуждение {}\nПротив: {}, за: {} + {} с замечаниями".format(id, result['not'], result['yes-ok'], result['yes-but']))
+    print("Номера замечаний «против»: {}".format(' '.join(map(str, result['not-ids']))))
 
-print(result)
+
+# Main run
+for id in files:
+    analyze_file(id, files[id])
 
